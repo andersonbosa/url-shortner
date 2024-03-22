45eabbebf8e01e90c36a4e3f5a39ac09d13e1144
andersonbosa <andersonbosa0@gmail.com>
2024-03-22 10:45:12 -0300

    style: modify logging configuration and file handling

--- url-shortner-app/src/config.ts
+++ url-shortner-app/src/config.ts
@@ -61,22 +61,31 @@ const defaultPostgresConfig: PostgresConfig = {
   database: 'url-shortner-db',
 }
 
-function generateLogFileName (): string {
-  const formatDate = (date: Date): string => {
+function generateLogFileName (
+  includeTime: boolean = false
+): string {
+
+  const formatDateToFile = (date: Date): string => {
     const year = date.getFullYear().toString().padStart(4, '0')
     const month = (date.getMonth() + 1).toString().padStart(2, '0')
     const day = date.getDate().toString().padStart(2, '0')
-    const hours = date.getHours().toString().padStart(2, '0')
-    const minutes = date.getMinutes().toString().padStart(2, '0')
-    const seconds = date.getSeconds().toString().padStart(2, '0')
-    return `${year}_${month}_${day}--${hours}_${minutes}_${seconds}.log`
+
+    if (includeTime) {
+      const hours = date.getHours().toString().padStart(2, '0')
+      const minutes = date.getMinutes().toString().padStart(2, '0')
+      const seconds = date.getSeconds().toString().padStart(2, '0')
+      return `${year}_${month}_${day}--${hours}_${minutes}_${seconds}.log`
+    }
+
+    return `${year}_${month}_${day}.log`
   }
-  return formatDate(new Date())
+
+  return formatDateToFile(new Date())
 }
 
 const defaultLoggingConfig: LoggingConfig = {
   level: 'debug',
-fileName: generateLogFileName()
+  fileName: generateLogFileName()
 }
 
 /* App Configuration  */
--- url-shortner-app/src/lib/logger.service.ts
+++ url-shortner-app/src/lib/logger.service.ts
@@ -8,23 +8,37 @@ const loggingConfig = config.logging
 const logsDir = join(__dirname, '..', '..', 'logs')
 const logFileName = join(logsDir, loggingConfig.fileName ?? 'winston.log')
 
+const customLogFormat = winston.format.printf(
+  (logged) => {
+    return `${new Date().toISOString()}: ${logged.level}: ${JSON.stringify(logged.message, null, 4)}`
+  }
+)
 
 const winstonOptions = {
+  prettyPrint: true,
+
+
   levels: winston.config.syslog.levels, /* rfc5424 */
   level: loggingConfig.level ?? 'debug',
 
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    // winston.format.printf(logged => `${logged.timestamp} ${logged.level}: ${logged.message}`)
+    winston.format.printf(logged => `${logged.timestamp} ${logged.level}: ${JSON.stringify(logged.message, undefined, 4)}`)
   ),
 
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: logFileName })
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize(), customLogFormat),
+    }),
+    new winston.transports.File({
+      filename: logFileName,
+      maxsize: 40_000,
+      maxFiles: 10,
+    })
   ]
 }
 
 const logger = winston.createLogger(winstonOptions)
-
 
 export default logger
