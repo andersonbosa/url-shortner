209876f1772a151bd8c1027353e1c9af290c85c5
andersonbosa <andersonbosa0@gmail.com>
2024-03-22 16:37:36 -0300

    feat: introduce Redis service for caching and data persistence

--- url-shortner-app/src/lib/redis.service.ts
+++ url-shortner-app/src/lib/redis.service.ts
@@ -1,73 +1,76 @@
-// import { URL } from 'url'
+import { RedisClientType, createClient } from 'redis'
+import { URL } from 'node:url'
+import logger from './logger.service'
 
-// import { RedisClientType, createClient } from 'redis'
+interface RedisServiceInput {
+  host: string
+  port: number
+  username: string
+  password: string
+}
 
+class RedisService {
+  private client: RedisClientType
 
-// interface RedisServiceInput {
-//   host: string
-//   port: number
-//   username: string
-//   password: string
-// }
+  constructor({ host, port, username, password }: RedisServiceInput) {
+    const clientUrl: string = this.generateUrl({
+      host: host,
+      port: port,
+      username: username,
+      password: password,
+    })
 
-// class RedisService {
-//   private client: RedisClientType
+    this.client = createClient({ url: clientUrl })
+  }
 
-//   constructor({ host, port, username, password }: RedisServiceInput) {
-//     const clientUrl: string = this.generateUrl({
-//       host: host,
-//       port: port,
-//       username: username,
-//       password: password,
-//     })
+  private generateUrl ({ host, port, username, password }: RedisServiceInput): string {
+    const urlObj = new URL('redis://')
+    urlObj.hostname = host
+    urlObj.port = port.toString()
+    urlObj.username = username
+    urlObj.password = password ?? ''
 
-//     this.client = createClient({ url: clientUrl })
-//   }
+    return urlObj.toString()
+  }
 
-//   private generateUrl ({ host, port, username, password }: RedisServiceInput): string {
-//     const urlObj = new URL('redis://')
-//     urlObj.hostname = host
-//     urlObj.port = port.toString()
-//     urlObj.username = username
-//     urlObj.password = password ?? ''
+  async connect (): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.client.on('connect', () => {
+        logger.debug('Conectado ao Redis!')
+        resolve()
+      })
+      this.client.on('error', (error: Error) => {
+        logger.error('Erro ao conectar ao Redis:', error)
+        reject(error)
+      })
+    })
+  }
 
-//     return urlObj.toString()
-//   }
+  async disconnect (): Promise<void> {
+    this.client.quit()
+    logger.debug('Desconectado do Redis!')
+  }
 
-//   async connect (): Promise<void> {
-//     return new Promise<void>((resolve, reject) => {
-//       this.client.on('connect', () => {
-//         console.log('Conectado ao Redis!')
-//         resolve()
-//       })
-//       this.client.on('error', (error: Error) => {
-//         console.error('Erro ao conectar ao Redis:', error)
-//         reject(error)
-//       })
-//     })
-//   }
+  async set (key: string, value: string): Promise<void> {
+    this.client.set(key, value)
+  }
 
-//   async disconnect (): Promise<void> {
-//     this.client.quit()
-//     console.log('Desconectado do Redis!')
-//   }
+  async get (key: any): Promise<string | null> {
+    return await this.client.get(key) 
+    // return new Promise<string | null>((resolve, reject) => {
+    //   this.client.get(
+    //     key,
+    //     (error: Error | null, result: string | null) => {
+    //       if (error) {
+    //         console.error('Erro ao obter valor do Redis:', error)
+    //         reject(error)
+    //       } else {
+    //         resolve(result)
+    //       }
+    //     }
+    //   )
+    // })
+  }
+}
 
-//   async set (key: string, value: string): Promise<void> {
-//     this.client.set(key, value)
-//   }
-
-//   async get (key: string): Promise<string | null> {
-//     return new Promise<string | null>((resolve, reject) => {
-//       this.client.get(key, (error: Error | null, result: string | null) => {
-//         if (error) {
-//           console.error('Erro ao obter valor do Redis:', error)
-//           reject(error)
-//         } else {
-//           resolve(result)
-//         }
-//       })
-//     })
-//   }
-// }
-
-// export default RedisService
+export default RedisService
