59417f7910c84f2da72b5ff3a4bccfb5b713f2cf
andersonbosa <andersonbosa0@gmail.com>
2024-03-23 10:56:39 -0300

    refactor: simplify setup function and remove setup postgres service class

--- url-shortner-app/src/setup.ts
+++ url-shortner-app/src/setup.ts
@@ -1,37 +1,25 @@
 import config from './config'
 import logger from './lib/logger.service'
-import PostgresService from './lib/postgres.service'
+import { createPostgreService } from './lib/postgres.service'
 
-const QUERIES = {
-  createTable: /* SQL */`
-    CREATE TABLE IF NOT EXISTS "url-shortner-db" (
-      id SERIAL PRIMARY KEY,
-      code TEXT UNIQUE,
-      original_url TEXT,
-      created_at timestamp DEFAULT CURRENT_TIMESTAMP
-    )
-  `
-}
 
-async function setup () {
-  const postgresService = new PostgresService(config.database.postgres)
+async function setup() {
+  const pgService = createPostgreService(config.database.postgres)
 
   try {
-    const isHealth = await postgresService.healthCheck()
-    if (!isHealth) {
-      throw new Error('postgresService.healthCheck failed')
-    }
-
-    await postgresService.connect()
-    await postgresService.query(QUERIES.createTable)
+    await pgService/* SQL */`
+        CREATE TABLE IF NOT EXISTS "url-shortner-db" (
+        id SERIAL PRIMARY KEY,
+        code TEXT UNIQUE,
+        original_url TEXT,
+        created_at timestamp DEFAULT CURRENT_TIMESTAMP
+        )
+    `
     logger.info('✅ Setup done successfully.')
 
   } catch (error) {
     logger.error('❌ Setup did not happen due to a error.')
     throw error
-
-  } finally {
-    await postgresService.disconnect()
   }
 }
 
